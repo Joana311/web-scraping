fe1d877b846a90f1e06ea1e7e1c975c6ead3c089
Julio Ramirez <julio1031.jr@gmail.com>
2022-09-04 00:34:59 -0700

    enable logging

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -152,8 +152,8 @@ export default withTRPC<AppRouter>({
         // adds pretty logs to your console in development and logs errors in production
         loggerLink({
           enabled: (opts) =>
-            process.env.NODE_ENV === "development" ||
-            (opts.direction === "down" && opts.result instanceof Error)
+            process.env.NODE_ENV === "development" || true
+          // || (opts.direction === "down" && opts.result! instanceof Error)
         }),
 
         // adds opt-out support for batching
