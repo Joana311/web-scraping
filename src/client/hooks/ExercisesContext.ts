55f3dbd8aabba865e655295298ec97a118397dbb
Julio Ramirez <julio1031.jr@gmail.com>
2022-09-24 19:22:17 -0700

    Delete ExercisesContext.ts

--- src/client/hooks/ExercisesContext.ts
+++
@@ -1,8 +0,0 @@
-// first populate an initial set of exercises from the server say 100.
-// then when the user searches, send a query to the server to perform the search
-// at the same time, filter the exercises in the store and set them to filteredExercises
-// when the fetch returns, add these exercises to the total set of exercises as well as the filtered set
-// need to make sure that duplicates aren't added. perhaps use a map (id -> exercise).
-// **maybe** -- or just keep a copy of the id and fetch exercises data
-
-
